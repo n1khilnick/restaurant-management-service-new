27d9062aeca90777abd0aebbe1cc52b8e9526080
N1khil <saininikhilnnn@gmail.com>
2023-05-21 10:51:40 +0530

    new

---
+++ src/main/java/com/geekster/restaurantmanagementservice/model/Admin.java
@@ -0,0 +1,35 @@
+package com.geekster.restaurantmanagementservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "admin")
+@Entity
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+
+    private String adminFirstName;
+
+    private String adminLastName;
+    @Email(regexp = "^[A-Za-z0-9._%+-]+@admin\\.com$",message = "email should be in format of user@admin.com")
+    @Column(nullable = false ,unique = true)
+    private String adminEmail;
+
+    @Column(nullable = false ,unique = true)
+    private String adminUserName;
+
+
+
+
+
+}
